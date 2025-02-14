2e2f73ec9597f74b96bf32eee4b8235bded89b96
Victor L. Castro <victorlcastro.dsa@gmail.com>
2025-02-14 09:08:35 -0300

    Add main entry point for asynchronous application execution with logging setup

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import asyncio
-import os
-import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import App
+from app.app import App
 from config.logger_config import LoggerConfig
 from config.settings import Settings
 
